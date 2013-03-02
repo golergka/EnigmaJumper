dd80265d039dc34fd47218003cd20007273147fb
Max Yankov <golergka@gmail.com>
2013-03-03 02:26:26 +0400

    Implemented timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+	public float time { get; private set; }
+
+	float startTime;
+
+	void OnGameReset() {
+
+		startTime = Time.time;
+
+	}
+
+	void OnGUI() {
+
+		string timeText;
+
+		int totalSeconds = Mathf.FloorToInt(time);
+		int seconds = totalSeconds % 60;
+        int minutes = totalSeconds / 60;
+
+		timeText = string.Format("{0}:{1:00}", minutes, seconds);
+
+		GUI.Label(new Rect(10,10,150,100), timeText);
+
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		GameController.instance.GameReset += OnGameReset;
+
+		OnGameReset();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!GameController.instance.gameOver)
+			time = Time.time - startTime;
+	
+	}
+}
